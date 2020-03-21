2fbc28db76b8784511808062e923b0db648d8f61
Randle Kehinde Olanrewaju <kennyrandle09@hotmail.co.uk>
2020-03-21 18:32:53 +0000

    auth added to middleware, identity pck installed, auth settings in startup

--- RiederBackend/Startup.cs
+++ RiederBackend/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,7 +13,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using RiederBackend.Helpers;
 using RiederBackend.Model;
+using RiederBackend.ServiceInterface;
+using RiederBackend.Services;
 
 namespace RiederBackend
 {
@@ -28,9 +34,40 @@ namespace RiederBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //-------------------
+            services.AddCors();
+            //--------------------
             //using in memory db for test
             //TODO: remember to change this to sqlserver in production
             services.AddDbContext<ReiderDbcontext>(db => db.UseInMemoryDatabase("bicycles"));
+
+            //configuring strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            //configuration jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            //configure DI for application services 
+            services.AddScoped<IUserServices, UserServices>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +80,18 @@ namespace RiederBackend
 
             app.UseHttpsRedirection();
 
+            //global cors policy
+            app.UseCors(x =>
+                x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod()
+
+            );
+            //--------end
+
             app.UseRouting();
+            //add the authentication to the middleware
+
+            app.UseAuthentication();
+            //--------------------end
 
             app.UseAuthorization();
 
