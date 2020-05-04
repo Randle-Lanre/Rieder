136b5fd0c8a19e13d6be786a9d4bada3957215b5
Randle Kehinde Olanrewaju <kennyrandle09@hotmail.co.uk>
2020-05-04 14:25:40 +0100

    added DTOS and skeleton for automapper profiles

--- RiederBackend/Controllers/BicyclesController.cs
+++ RiederBackend/Controllers/BicyclesController.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RiederBackend.Model;
 
 namespace RiederBackend.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class BicyclesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ILogger<BicyclesController> _logger;
 
-        public BicyclesController(ApplicationDbContext context)
+        public BicyclesController(ApplicationDbContext context, IMapper mapper, ILogger<BicyclesController> logger)
         {
             _context = context;
+            _mapper = mapper;
+            _logger = logger;
         }
 
+        public 
        
     }
 }
---
+++ RiederBackend/Helpers/AutomapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace RiederBackend.Helpers
+{
+    public class AutomapperProfiles: Profile
+    {
+        public AutomapperProfiles()
+        {
+
+            //TODO: configure mappings
+            
+        }
+    }
+}
