eb7949a2798f31a07ab2e47dc2679cfe5c4c50b4
Olanrewaju Randle <kennyrandle09@hotmail.co.uk>
2020-11-23 22:33:09 +0000

    added comp sek

--- rieder-frontend/src/app/app.module.ts
+++ rieder-frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterPageComponent } from './footer-page/footer-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { BikesComponent } from './bikes/bikes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LandingPageComponent,
+    TestimonialsComponent,
+    FooterPageComponent,
+    ContactPageComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UserAccountComponent,
+    BikesComponent
   ],
   imports: [
     BrowserModule,
