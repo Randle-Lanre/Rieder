e8788f7da2ea8af76ba88ca3d6f777ea7fd76d33
Olanrewaju Randle <kennyrandle09@hotmail.co.uk>
2020-12-12 04:19:48 +0000

    mdf login comp design

--- rieder-frontend/src/app/app.module.ts
+++ rieder-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,9 @@ import { BikesComponent } from './bikes/bikes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- rieder-frontend/src/app/login/login.component.ts
+++ rieder-frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { from } from 'rxjs';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  
+   loginForm: FormGroup ;
+
+  
+
+  constructor(private fb: FormBuilder ) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    })
+  
+    
+   }
+  
+  
 
   ngOnInit(): void {
+    
+this.loginForm
+    
   }
 
-}
+
+  //click submission even 
+
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
+ }
