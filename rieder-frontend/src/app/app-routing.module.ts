62e5cdb2957e2203aa4bff11382e67fb5fc11c2e
Olanrewaju Randle <kennyrandle09@hotmail.co.uk>
2020-12-13 08:53:49 +0000

    migrated to bootstrap 5

--- rieder-frontend/src/app/app-routing.module.ts
+++ rieder-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'contactUs', component: ContactPageComponent}
 ];
 
 @NgModule({
