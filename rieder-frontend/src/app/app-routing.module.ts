a0007e3c1bcf3f139b0e6a7e70254d18611edfec
Olanrewaju Randle <kennyrandle09@hotmail.co.uk>
2021-01-10 15:15:42 +0000

    user-account init design and assests

--- rieder-frontend/src/app/app-routing.module.ts
+++ rieder-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
+import { UserAccountComponent } from './user-account/user-account.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'contactUs', component: ContactPageComponent}
+  {path: 'contactUs', component: ContactPageComponent},
+  {path: 'account', component: UserAccountComponent}
 ];
 
 @NgModule({
